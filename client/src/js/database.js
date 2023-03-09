4a6bb70364d103326cf43030c92b46ca1330c006
unknown <aguilera.k02@gmail.com>
2023-03-08 23:45:49 -0500

    added put and get requests to db

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -12,10 +12,32 @@ const initdb = async () =>
     },
   });
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+  try {
+  const todosDb = await openDB('text', 1);
+  const tx = todosDb.transaction('text', 'readwrite');
+  const store = tx.objectStore('text');
+  const request = store.put({ id: id, text: content });
+  const result = await request;
+  console.log('data saved to the database', result);    
+  } catch {
+    console.error('putDb not implemented');  
+  }
+}
 
-// TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
+  try {
+  const textDb = await openDB('text', 1);
+  const tx = textDb.transaction('text', 'readonly');
+  const store = tx.objectStore("text");
+  const request = store.getAll();
+  const result = await request;
+  console.log('result.value', result);
+  return result;    
+  } catch {
+  console.error('getDb not implemented');
+  }
+}
+
 
 initdb();
